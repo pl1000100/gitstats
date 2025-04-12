8622cb5e59bb084516eb467d92b07a2fe983312b
pl1000100 <olszynski97@gmail.com>
2025-04-12 10:29:41 +0200

    Added CORS middleware

--- backend/main.go
+++ backend/main.go
@@ -15,6 +15,24 @@ type Config struct {
 	GitHubToken string
 }
 
+func corsMiddleware(next func(http.ResponseWriter, *http.Request)) func(http.ResponseWriter, *http.Request) {
+	return (func(w http.ResponseWriter, r *http.Request) {
+		// Allow CORS from the frontend (localhost:80)
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost") // Change this as necessary for other environments
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		// If the method is OPTIONS, respond with status 200 and return
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		// Proceed with the next handler
+		next(w, r)
+	})
+}
+
 func main() {
 	// Load the .env file from the parent directory
 	err := godotenv.Load()
@@ -32,9 +50,9 @@ func main() {
 	githubClient := github.NewGitHubAPIClient(c.GitHubToken)
 
 	mux := http.NewServeMux()
-
+	mux.HandleFunc("GET /api/v1/stats/{user}/{repo}", corsMiddleware(count_loc.HandleStats))
 	mux.HandleFunc("GET /api/v1/repositories/{name}", githubClient.HandleRepositories)
-	mux.HandleFunc("GET /api/v1/stats/{user}/{repo}", count_loc.HandleStats)
+	// mux.HandleFunc("GET /api/v1/stats/{user}/{repo}", corsMiddleware(count_loc.HandleStats))
 	mux.HandleFunc("GET /api/v1/stats/{user}", count_loc.HandleStatsAll(*githubClient))
 
 	log.Default().Print("Server running")
